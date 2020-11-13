9329e4dbde965bae3d241a9b8f3abe6d7c175e6e
Alexander Trost <galexrt@googlemail.com>
2020-11-13 17:57:51 +0100

    allow custom_data to be copied to /data in base entrypoint
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- container/base/rootfs/scripts/entrypoint.sh
+++ container/base/rootfs/scripts/entrypoint.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 JAVA_JAR="${JAVA_JAR:-}"
+JAVA_FLAGS="${JAVA_FLAGS:-}"
+
+if [ -d /custom_data/ ]; then
+    echo "Copying /custom_data dir to /data"
+    cp -ar /custom_data/ /data/
+fi
 
 if [ "${1}" = "java" ]; then
     echo "Running java jar because first arg ist 'java' ..."
     shift
+
+    if [ -n "${JAVA_FLAGS}" ]; then
+        echo "Adding JAVA_FLAGS to args: ${JAVA_FLAGS}"
+        set -- "${JAVA_FLAGS}" "$@"
+    fi
     if [ -n "${JAVA_JAR}" ]; then
         set -- -jar "${JAVA_JAR}" "$@"
     else
@@ -15,5 +26,4 @@ if [ "${1}" = "java" ]; then
 fi
 
 echo "Running arbitrary command ..."
-
 exec "${@}"
