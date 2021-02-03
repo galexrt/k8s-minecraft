1c8c513552bc62d9cebd36c65721efe72a9b393a
Alexander Trost <galexrt@googlemail.com>
2021-02-03 23:27:48 +0100

    skip empty lines in plugins list
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- container/base/rootfs/custom_scripts/pre/plugins-install.sh
+++ container/base/rootfs/custom_scripts/pre/plugins-install.sh
@@ -25,4 +25,4 @@ while IFS= read -r plugin; do
 
     # shellcheck disable=SC2145,SC2086
     rsync ${RSYNC_FLAGS} "${CUSTOM_SCRIPT_PLUGINS_DIR}/${plugin}" /data/plugins
-done < "/data/.plugins_install_list.txt"
+done < <(sed '/^$/d' "/data/.plugins_install_list.txt")
