7e94cd3ab22f6076f4870373d7ebc148e20141ec
Alexander Trost <galexrt@googlemail.com>
2021-02-11 20:35:33 +0100

    add server config install copy script
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

---
+++ container/base/rootfs/custom_scripts/pre/server-configs-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+POD_HOSTNAME="$(cat /etc/hostname)"
+SERVER_NAME="${POD_HOSTNAME%-*}"
+CUSTOM_SCRIPT_SERVER_CONFIGS_INSTALL="${CUSTOM_SCRIPT_SERVER_CONFIGS_INSTALL:-false}"
+CUSTOM_SCRIPT_SERVER_CONFIGS_DIR="${CUSTOM_SCRIPT_SERVER_CONFIGS_DIR:-/repo/servers}"
+RSYNC_FLAGS="${RSYNC_FLAGS:--aurv}"
+
+if [ "${CUSTOM_SCRIPT_SERVER_CONFIGS_INSTALL}" != "true" ]; then
+    echo "Skipping server config data install."
+    exit
+fi
+
+if [ -d "${CUSTOM_SCRIPT_SERVER_CONFIGS_DIR}/${SERVER_NAME}/${POD_HOSTNAME}" ]; then
+    echo "Copying ${POD_HOSTNAME} server config data to server ..."
+    # shellcheck disable=SC2145,SC2086
+    rsync --dry-run ${RSYNC_FLAGS} "${CUSTOM_SCRIPT_SERVER_CONFIGS_DIR}/${SERVER_NAME}/${POD_HOSTNAME}/" /data/
+    echo "ERROR: Failed to copy ${POD_HOSTNAME} server config data to server. Sleeping 5 seconds ..."
+    sleep 5
+else
+    echo "ERROR: Failed to find ${POD_HOSTNAME} server config data dir in ${CUSTOM_SCRIPT_SERVER_CONFIGS_DIR}/${SERVER_NAME}/. Sleeping 20 seconds ..."
+    sleep 20
+fi
