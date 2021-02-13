3eaba5b6ce7ac6d5e00fadcc4f83d65b61254b07
Alexander Trost <galexrt@googlemail.com>
2021-02-13 18:55:16 +0100

    tweak server-configs-install.sh script
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- container/base/rootfs/custom_scripts/pre/server-configs-install.sh
+++ container/base/rootfs/custom_scripts/pre/server-configs-install.sh
@@ -14,17 +14,17 @@ if [ "${CUSTOM_SCRIPT_SERVER_CONFIGS_INSTALL}" != "true" ]; then
 fi
 
 if [ -d "${CUSTOM_SCRIPT_SERVER_CONFIGS_DIR}/${SERVER_NAME}/data" ]; then
-    echo "Copying ${POD_HOSTNAME} server config data to server ..."
+    echo "Copying ${SERVER_NAME}/data server config data to server ..."
     # shellcheck disable=SC2145,SC2086
-    rsync --dry-run ${RSYNC_FLAGS} "${CUSTOM_SCRIPT_SERVER_CONFIGS_DIR}/${SERVER_NAME}/data/" /data/
+    rsync ${RSYNC_FLAGS} "${CUSTOM_SCRIPT_SERVER_CONFIGS_DIR}/${SERVER_NAME}/data/" /data/
 else
-    echo "Skipping ${SERVER_NAME} server config data install, no dir found in ${CUSTOM_SCRIPT_SERVER_CONFIGS_DIR}/${SERVER_NAME}/data/ ..."
+    echo "Skipping ${SERVER_NAME}/data server config data install, no dir found in ${CUSTOM_SCRIPT_SERVER_CONFIGS_DIR}/${SERVER_NAME}/data/ ..."
 fi
 
 if [ -d "${CUSTOM_SCRIPT_SERVER_CONFIGS_DIR}/${SERVER_NAME}/${POD_HOSTNAME}" ]; then
     echo "Copying ${POD_HOSTNAME} server config data to server ..."
     # shellcheck disable=SC2145,SC2086
-    rsync --dry-run ${RSYNC_FLAGS} "${CUSTOM_SCRIPT_SERVER_CONFIGS_DIR}/${SERVER_NAME}/${POD_HOSTNAME}/" /data/
+    rsync ${RSYNC_FLAGS} "${CUSTOM_SCRIPT_SERVER_CONFIGS_DIR}/${SERVER_NAME}/${POD_HOSTNAME}/" /data/
     sleep 5
 else
     echo "Skipping ${POD_HOSTNAME} server config data install, no dir found in ${CUSTOM_SCRIPT_SERVER_CONFIGS_DIR}/${SERVER_NAME}/${POD_HOSTNAME}/ ..."
