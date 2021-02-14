86d162ee22547b8e5c15c82f524b51354151706e
Alexander Trost <galexrt@googlemail.com>
2021-02-14 13:11:39 +0100

    make envsubst script smarter
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- container/base/rootfs/custom_scripts/post/envsubst.sh
+++ container/base/rootfs/custom_scripts/post/envsubst.sh
@@ -12,37 +12,41 @@ GAMESERVER_POD_HOSTNAME="${GAMESERVER_POD_HOSTNAME:-${POD_HOSTNAME}}"
 GAMESERVER_SERVER_TYPE="${GAMESERVER_SERVER_TYPE:-unset}"
 
 echo "envsubst: Running envsubst on config files ..."
-for var in "${!GAMESERVER_@}"; do
-    echo "Replacing var ${var} in files ..."
-    find /data/*.yml /data/plugins \
+find /data/*.yml /data/plugins \
+    \( \
         \( \
             \( \
-                \( \
-                    -iwholename '/data/plugins/dynmap/web/tiles' \
-                    -o -iwholename '/data/plugins/Essentials/userdata' \
-                \) \
-                -prune -false \
+                -iwholename '/data/plugins/dynmap/web/tiles' \
+                -o -iwholename '/data/plugins/Essentials/userdata' \
             \) \
-           -o \( \
-                -type f \
-                -a \( \
-                    \( -iname '*.yml' \) \
-                    -o \( -iname '*.yaml' \) \
-                    -o \( -iname '*.txt' \) \
-                    -o \( -iname '*.properties' \) \
-                    -o \( -iname '*.conf' \) \
-                    -o \( -iname '*.json' \) \
-                \) \
-            \) -print0 \
-        \) | \
-            while IFS= read -r -d '' file; do
+            -prune -false \
+        \) \
+       -o \( \
+            -type f \
+            -a \( \
+                \( -iname '*.yml' \) \
+                -o \( -iname '*.yaml' \) \
+                -o \( -iname '*.txt' \) \
+                -o \( -iname '*.properties' \) \
+                -o \( -iname '*.conf' \) \
+                -o \( -iname '*.json' \) \
+            \) \
+        \) -print0 \
+    \) | \
+        while IFS= read -r -d '' file; do
+            # If file doesn't contain any `GAMESERVER_*` vars, skip it early
+            if ! grep --quiet "\${GAMESERVER_*}" "${file}" > /dev/null 2>&1; then
+                continue
+            fi
+            for var in "${!GAMESERVER_@}"; do
                 value="${!var}"
                 # In value replacements are done here
                 value="${value//\%POD_ID\%/${POD_ID}}"
                 value="${value//\%POD_ID_PLUS\%/${POD_ID_PLUS}}"
 
-                # Sed replacement
+                echo "Replacing ${var} in ${file} ..."
                 sed -i 's#${'"${var}"'}#'"${value}"'#g' "${file}"
             done
-done
+        done
+
 echo "envsubst: Done envsubst on config files."
