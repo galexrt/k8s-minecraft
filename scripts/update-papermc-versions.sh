537908053f9a708cfe73ce1aaa1428ba91bf2850
Alexander Trost <galexrt@googlemail.com>
2022-10-01 20:01:49 +0200

    use new papermc api endpoint
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- scripts/update-papermc-versions.sh
+++ scripts/update-papermc-versions.sh
@@ -11,10 +11,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}" || exit 1
 
 # Paper
-PAPER_MC_VERSION=$(curl -sS --fail "https://papermc.io/api/v2/projects/paper/" | \
+PAPER_MC_VERSION=$(curl -sS --fail "https://api.papermc.io/v2/projects/paper/" | \
     jq -r '.versions | reverse | .[] | select(startswith("'"${MC_VERSION}"'"))' | head -n1)
 
-PAPER_VERSION=$(curl -sS --fail "https://papermc.io/api/v2/projects/paper/versions/${PAPER_MC_VERSION}/" | \
+PAPER_VERSION=$(curl -sS --fail "https://api.papermc.io/v2/projects/paper/versions/${PAPER_MC_VERSION}/" | \
     jq -r '.builds[-1]')
 
 if ! grep -q "${PAPER_MC_VERSION}/${PAPER_VERSION}" ../container/paper/Makefile; then
@@ -26,7 +26,7 @@ if ! grep -q "${PAPER_MC_VERSION}/${PAPER_VERSION}" ../container/paper/Makefile;
 fi
 
 # Waterfall
-WATERFALL_VERSION=$(curl -sS --fail "https://papermc.io/api/v2/projects/waterfall/versions/${MC_VERSION}/" | \
+WATERFALL_VERSION=$(curl -sS --fail "https://api.papermc.io/v2/projects/waterfall/versions/${MC_VERSION}/" | \
     jq -r '.builds[-1]')
 
 if ! grep -q "${MC_VERSION}/${WATERFALL_VERSION}" ../container/waterfall/Makefile; then
